6eb8ba97693553206533d5d88927420a91772a12
Chad <chawagon03@gmail.com>
2024-11-15 18:38:11 -0500

    ADD: docker-install.sh

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # garagelab-utils
+
+# DOCKER
+
+## INSTALL
+
+```bash
+curl -sL -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/chadwagoner/garagelab-utils/main/docker-install.sh | bash
+```
---
+++ docker-install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+### REMOVE OLD PACKAGES
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+### ADD DOCKER OFFICIAL GPG KEY
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+### ADD DOCKER REPOSITORY TO APT
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+### INSTALL DOCKER
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+### ADD USER TO DOCKER GROUP
+sudo usermod -aG docker $USER
+
+### START DOCKER AT BOOT
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
