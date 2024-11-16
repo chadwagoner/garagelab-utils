84ad36cb4f6b6e584ac0efdf6cb930b148235240
Chad <chawagon03@gmail.com>
2024-11-15 19:51:26 -0500

    should fix formatting

--- os-updater-install.sh
+++ os-updater-install.sh
@@ -2,21 +2,19 @@
 
 ### OS-UPDATER
 os_updater=$(cat << 'EOF'
-apt-get update &&
-apt-get upgrade &&
-apt-get dist-upgrade &&
-apt-get autoremove &&
+apt-get update && \
+apt-get upgrade && \
+apt-get dist-upgrade && \
+apt-get autoremove && \
 apt-get autoclean
+
+/usr/bin/sleep 60
+
+if [[ -f /var/run/reboot-required ]]; then
+  shutdown -r now
+fi
 EOF
 )
 
 ### INSTALL OS-UPDATER
-echo $os_updater | sudo tee /etc/cron.monthly/os-updater >/dev/null
-
-### PAUSE FOR 60 SECONDS
-/usr/bin/sleep 60
-
-### REBOOT IF REQUIRED
-if [[ -f /var/run/reboot-required ]]; then
-  shutdown -r now
-fi
+echo "$os_updater" | sudo tee /etc/cron.monthly/os-updater >/dev/null
